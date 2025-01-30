aa7a715c69c54f15a9992c40024e181da16e9e20
ChihiroIsmail <ismail@ids.co.jp>
2025-01-30 15:44:37 +0900

    solve pylance error

--- SNS_SQS_Lambda/localstack/src/lambda_function.py
+++ SNS_SQS_Lambda/localstack/src/lambda_function.py
@@ -1,15 +1,13 @@
 import json
 
+
 def lambda_handler(event, context):
     try:
         print("Received event: " + json.dumps(event, indent=2))
 
-        for record in event['Records']:
-            print("Message Body: " + record['body'])
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Hello from Lambda!')
-        }
+        for record in event["Records"]:
+            print("Message Body: " + record["body"])
+        return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
     except Exception as e:
         print("Error processing event: " + str(e))
         raise e
